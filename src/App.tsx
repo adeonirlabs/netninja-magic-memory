ebb0554509b94465421b635a84e2cdd1798dd5bb
Adeonir Kohl <adeonir@gmail.com>
2022-06-24 22:42:49 -0300

    Add shuffle cards function

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
+import { useState } from 'react'
 import './App.scss'
 
+const cardImages = [
+  { src: '/assets/helmet.png' },
+  { src: '/assets/potion.png' },
+  { src: '/assets/ring.png' },
+  { src: '/assets/scroll.png' },
+  { src: '/assets/shield.png' },
+  { src: '/assets/sword.png' },
+]
+
+type Card = {
+  src: string
+  id: number
+}
+
 export function App() {
+  const [cards, setCards] = useState<Card[]>([])
+  const [turns, setTurns] = useState(0)
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card, index) => ({ ...card, id: index }))
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
+  console.log(cards)
+  console.log(turns)
+
   return (
     <div className="App">
       <h1>Magic Memory</h1>
-      <button>New game</button>
+      <button onClick={shuffleCards}>New game</button>
     </div>
   )
 }
